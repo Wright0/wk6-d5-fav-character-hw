3bf270bc6cd160fa8197da6679cf94445f64f0ab
Olivia <olivia.wright101@gmail.com>
2020-01-11 12:32:44 +0000

    app.js planning

---
+++ js/app.js
@@ -0,0 +1,20 @@
+document.eventListenner('DOMContentLoaded', () => {
+
+//Listen for form being submitted
+////call handleFormSubmittion function
+////call createDeleteButton function
+
+//Listen for delete button being clicked
+////call delete list function
+
+});
+
+//Function to create list items
+
+//Function to handle form submission
+
+//Function that checks length of items in list (This will decide if a delete button should be added or not. If the node length is 0, add one. If it's 1, don't. This *should* ensure that only 1 delete button is added.)
+
+//function that adds a delete button (calling node length check)
+
+//Function to handle deletion of list
